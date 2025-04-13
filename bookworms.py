18219edc7b58f3099ac012667c9c39cbf98102b4
MrTaco98 <juan.sosa76880@gmail.com>
2025-04-12 20:59:42 -0500

    Added AWS

--- bookworms.py
+++ bookworms.py
@@ -1,7 +1,25 @@
 from flask import Flask, render_template
-
+import pymysql.cursors
+ 
 app = Flask(__name__)
+ 
+app.config['MYSQL_DATABASE_HOST'] = 'bookworms-flask.c9e4q2aoy2op.us-east-2.rds.amazonaws.com' # Specify Endpoint
+app.config['MYSQL_DATABASE_USER'] = 'admin' # Specify Master username
+app.config['MYSQL_DATABASE_PASSWORD'] = 'administrator' # Specify Master password
+app.config['MYSQL_DATABASE_DB'] = 'bookworms-flask' # Specify database name
+ 
+connection = pymysql.connect(host='bookworms-flask.c9e4q2aoy2op.us-east-2.rds.amazonaws.com',
+                             user='admin',
+                             password='administrator',
+                             database='bookworms',
+                             cursorclass=pymysql.cursors.DictCursor)
 
+with connection:
+    with connection.cursor() as cursor:
+        cursor.execute(''' SELECT * FROM test ''')
+        data=cursor.fetchone()
+        print(data)
+     
 @app.route("/")
 @app.route("/home")
 def home():
