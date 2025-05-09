2f80bd061eb1a6a8967d447fac239fb19d920a8e
Alfred <42361881+Xyloidzzz@users.noreply.github.com>
2025-05-08 19:07:28 -0500

    added new edit table and selection bias
    EDIT NOT YET IMPLEMENTED DON'T CLICK BUTTON

--- static/scripts/tables.js
+++ static/scripts/tables.js
@@ -1,13 +1,19 @@
-var trs = document.getElementsByTagName('tr')
+var trs = document.getElementsByTagName('tr');
+var addForm = document.getElementById('add-form');
+var editForm = document.getElementById('edit-form');
 
 for (var i = 0; i < trs.length; i++) {
     trs[i].onclick = (event) => {
       if (!event.currentTarget.classList.contains('active-row')) {
         removeAll('active-row');
         event.currentTarget.classList.add('active-row');
+        addForm.setAttribute('hidden', 'true');
+        editForm.removeAttribute('hidden');
       }
       else {
         event.currentTarget.classList.remove('active-row')
+        editForm.setAttribute('hidden', 'true');
+        addForm.removeAttribute('hidden');
       }
     };
 }
